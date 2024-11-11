1c267360caae0fa860df06e2ef1a6ab207501ce8
Victorriv <Victorrivera714@yahoo.com>
2024-11-11 07:00:22 -0800

    math.floor so we dont get decimals

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-let randomNum = Math.random() * 6; // this will generate a random number between 0 and 1... Multipling 6 will give us a number between 0-6 exclusive. 
+let randomNum = Math.floor(Math.random() * 6); // this will generate a random number between 0 and 1... Multipling 6 will give us a number between 0-6 exclusive. 
+// math.floor will help us not get decimals. now it will give 1-5
+
+// but i want 1-6

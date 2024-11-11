09dabd5f8f9cf8ae3fce6dd9388f6c5f61fc3ad3
Victorriv <Victorrivera714@yahoo.com>
2024-11-11 07:28:49 -0800

    added one so i can get 1-6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let randomNum = Math.floor(Math.random() * 6); // this will generate a random number between 0 and 1... Multipling 6 will give us a number between 0-6 exclusive. 
+let randomNum = Math.floor(Math.random() * 6) +1; // this will generate a random number between 0 and 1... Multipling 6 will give us a number between 0-6 exclusive. 
 // math.floor will help us not get decimals. now it will give 1-5
 
-// but i want 1-6
+
+// but i want 1-6 so i increase the minimum by adding 1
 console.log(randomNum);

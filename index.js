b7c0488c8ebd82042736da40dc9dba28fef59398
Victorriv <Victorrivera714@yahoo.com>
2024-11-12 05:26:40 -0800

    for operator precedence

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const max =100;
 
 
 
-let randomNum = Math.floor(Math.random() * 100) +1; // this will generate a random number between 0 and 1... Multipling 6 will give us a number between 0-6 exclusive. 
+let randomNum = Math.floor(Math.random() * (max-min)) + min; // this will generate a random number between 0 and 1... Multipling 6 will give us a number between 0-6 exclusive. 
 // math.floor will help us not get decimals. now it will give 1-5
 // now it'll be 1-100 instead of 1-6
 
